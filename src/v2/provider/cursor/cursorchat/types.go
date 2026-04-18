13d4ae4f5dca19a1cca736f4bfb04703f265e87c
ZackarySantana <64446617+ZackarySantana@users.noreply.github.com>
2026-04-18 17:03:37 -0400

    refactor: cursorchat file structure

--- src/v2/provider/cursor/cursorchat/types.go
+++ src/v2/provider/cursor/cursorchat/types.go
@@ -277,3 +277,30 @@ func (e *ToolCallEvent) ToCanonical() (chat.Event, error) {
 }
 
 func (e *ToolCallEvent) event() {}
+
+type EventContent interface {
+	content()
+}
+
+type RawEventContent struct {
+	Type string `json:"type"`
+
+	Raw json.RawMessage `json:"-"`
+}
+
+func (e *RawEventContent) UnmarshalJSON(data []byte) error {
+	// Use an alias to avoid infinite recursion.
+	type Alias RawEventContent
+	if err := json.Unmarshal(data, (*Alias)(e)); err != nil {
+		return err
+	}
+	// Copy the data to its own memory, we cannot use data directly.
+	e.Raw = append(json.RawMessage(nil), data...)
+	return nil
+}
+
+type EventContentText struct {
+	Text string `json:"text"`
+}
+
+func (e *EventContentText) content() {}
