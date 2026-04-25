9aa2e95b84cd4267cdb465ecc09302a9d7b48c3c
ZackarySantana <64446617+ZackarySantana@users.noreply.github.com>
2026-04-24 21:25:13 -0400

    feat: link up components to studio package

--- packages/studio/src/index.tsx
+++ packages/studio/src/index.tsx
@@ -2,11 +2,13 @@ import { QueryClientProvider } from "@tanstack/solid-query";
 import { render } from "solid-js/web";
 import "./style.css";
 import { queryClient } from "./lib/query-client";
+import { Button } from "@moondust/components";
 
 render(
     () => (
         <QueryClientProvider client={queryClient}>
             <div>Hello World</div>
+            <Button>Click me</Button>
         </QueryClientProvider>
     ),
     document.getElementById("root")!,
